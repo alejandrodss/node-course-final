9df3c8b9cffe2b9c8f1c9dace953baa383494176
Alejandro Duarte <alejandrod.92@gmail.com>
2024-05-05 17:07:55 -0500

    Add Seeder

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { Options } from '@mikro-orm/core';
 import { PostgreSqlDriver } from "@mikro-orm/postgresql";
 import { Migrator } from '@mikro-orm/migrations';
+import { SeedManager } from '@mikro-orm/seeder';
 
 const config: Options<PostgreSqlDriver> = {
   driver: PostgreSqlDriver,
@@ -11,7 +12,15 @@ const config: Options<PostgreSqlDriver> = {
     path: './build/migrations', // path to the folder with migrations
     pathTs: './src/migrations', // path to the folder with TS migrations (if used, we should put path to compiled files in `path`)
   },
-  extensions: [Migrator]
+  extensions: [Migrator, SeedManager],
+  seeder: {
+    path: './build/seeders',
+    pathTs: './src/seeders',
+    defaultSeeder: 'DatabaseSeeder',
+    glob: '!(*.d).{js,ts}',
+    emit: 'ts',
+    fileName: (className: string) => className
+  }
 };
 
 export default config;
